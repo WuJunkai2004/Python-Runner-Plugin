67b9388c3a27faa36ba7fd74dee689c3bc2ce200
Wu Junkai <wu.junkai@qq.com>
2024-06-04 16:00:18 +0800

    feat: 完成通义千言

--- runner.js
+++ runner.js
@@ -2,17 +2,21 @@ function get_site_info(){
     let url = window.location.href;
     let support = [
         {
-            'site': 'baidu',
+            'site': 'yiyan',
             'id': '#DIALOGUE_CONTAINER_ID',
             "cmd": "for_baidu;"
         },{
-            'site': 'aliyun',
+            'site': 'tongyi',
             'id': '#chat-content',
             "cmd": "for_aliyun;"
         },{
             'site': 'gemini',
             'id': '.content-wrapper',
             "cmd": "for_gemini;"
+        },{
+            "site": "xfyun", // 讯飞星火
+            "id": "#chat-content",
+            "cmd": "for_xinghuo;"
         }
     ]
     for(let site of support){
@@ -58,7 +62,7 @@ function for_baidu(){
         let tr_list = code.getElementsByTagName('tr');
         let code_text = `def print(*args,sep=' ', end='\\n', file='', flush='', init=False):\n` +
                         '    from browser import document\n' +
-                        '    text = sep.join(list(map(str, args)))\n' +
+                        '    text = "  " + sep.join(list(map(str, args)))\n' +
                         '    if init:\n' +
                         `        document['${id}_output'].innerHTML = ''\n` +
                         '        return\n' +
@@ -76,7 +80,7 @@ function for_baidu(){
             try{
                 __BRYTHON__.runPythonSource(code_text, id);
             } catch(e) {
-                document.getElementById(id + '_output').innerHTML += '\nError: 因错误而退出\n';
+                document.getElementById(id + '_output').innerHTML += '\n  Error: 因错误而退出\n';
                 document.getElementById(id).hidden = false;
                 console.log(e);
             }
@@ -97,52 +101,59 @@ function for_aliyun(){
             parent.insertBefore(new_node, target_node.nextSibling);
         }
     };
-    let code_block = document.querySelectorAll('.code-block ng star-inserted');
+    let code_block = document.querySelectorAll('.tongyi-ui-highlighter');
     for(let code of code_block){
         let is_done = code.getAttribute('is-done') || null;
         if(is_done){
             continue;
         }
         code.setAttribute('is-done', true);
-        // add the display block
+        // 排除掉不是python的代码块
+        if(code.querySelector('span').innerText !== "Python"){
+            continue;
+        }
+        // 添加显示结果的代码块
         let id = Math.random().toString(36).slice(-8);
         let result = code.parentNode.cloneNode(true);
         result.setAttribute('id', id);
-        result.querySelectorAll('.mat-mdc-button-touch-target').forEach(element => element.remove());
-        result.querySelector('.code-block-decoration header gmat-subtitle-1 ng-star-inserted').innerText = 'output';
-        result.querySelector('tbody').innerHTML = '';
-        result.querySelector('tbody').setAttribute('id', id + '_output');
+        result.querySelectorAll('.tongyi-ui-highlighter-copy-btn').forEach(element => element.remove());
+        result.querySelector('.tongyi-ui-highlighter-lang').innerHTML = 'Output';
+        result.querySelector('code').innerHTML = '';
+        result.querySelector('code').setAttribute('id', id + '_output');
         result.setAttribute('hidden', true);
         insert_after(result, code.parentNode);
         // get the code script
-        let tr_list = code.getElementsByTagName('tr');
+        let text_list = code.querySelector('code').innerText.split('\n');
         let code_text = `def print(*args,sep=' ', end='\\n', file='', flush='', init=False):\n` +
                         '    from browser import document\n' +
-                        '    text = sep.join(list(map(str, args)))\n' +
+                        '    text = " " + sep.join(list(map(str, args)))\n' +
                         '    if init:\n' +
                         `        document['${id}_output'].innerHTML = ''\n` +
                         '        return\n' +
                         `    document['${id}'].hidden = False\n` +
                         `    document['${id}_output'].innerHTML += text + end\n` +
-                        'print(init=True)\n'
-        for(let tr of tr_list){
-            code_text += tr.innerText.slice(1) + '\n';
+                        'print(init=True)\n';
+        for(let line_id=1; line_id<=text_list.length; line_id++){
+            let id_len = line_id.toString().length;
+            code_text += text_list[line_id-1].slice(id_len) + '\n';
         }
         // show the run cpy_button
-        let cpy_button = code.getElementsByClassName('code-copy')[0];
+        let cpy_button = code.querySelector('svg');
         let run_button = cpy_button.cloneNode(true);
+        run_button.setAttribute('style', 'margin-left: 10px;');
+        run_button.querySelector('path').setAttribute('d', generate_run_svg(12));
         run_button.addEventListener('click', () => {
             console.log(code_text);
             try{
                 __BRYTHON__.runPythonSource(code_text, id);
             } catch(e) {
-                document.getElementById(id + '_output').innerHTML += '\nError: 因错误而退出\n';
+                document.getElementById(id + '_output').innerHTML += '\n Error: 因错误而退出\n';
                 document.getElementById(id).hidden = false;
                 console.log(e);
             }
         });
-        run_button.getElementsByClassName('code-copy-text')[0].innerText = '运行代码';
         insert_after(run_button, cpy_button);
+        console.log('done');
     }
 }
 
